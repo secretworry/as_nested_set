8a2405552ba2c57bf58aaca318134b126e711d52
Rainer Du <dusiyh@gmail.com>
2016-06-07 18:00:29 +0800

    Added Scoped

---
+++ lib/as_nested_set/scoped.ex
@@ -0,0 +1,24 @@
+defmodule AsNestedSet.Scoped do
+  defmacro __using__(args) do
+    quote do
+      @scope unquote(Keyword.get(args, :scope, []))
+      @before_compile AsNestedSet.Scoped
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    scope = Module.get_attribute(env.module, :scope)
+    quote do
+      @spec same_scope(any, any) :: boolean
+      def same_scope(source, target) do
+        AsNestedSet.Scoped.do_same_scope(source, target, unquote(scope))
+      end
+    end
+  end
+
+  def do_same_scope(source, target, scope) do
+    Enum.all?(scope, fn field ->
+      Map.get(source, field) == Map.get(target, field)
+    end)
+  end
+end
---
+++ test/as_nested_set/scoped_test.exs
@@ -0,0 +1,20 @@
+defmodule AsNestedSet.ScopedTest do
+  use ExUnit.Case
+  doctest AsNestedSet.Scoped
+  defmodule Sample do
+    use AsNestedSet.Scoped, scope: [:scope_field]
+    defstruct scope_field: nil, non_scope_field: nil
+  end
+
+  test "should export same_scope/2" do
+    Sample.same_scope(%Sample{}, %Sample{})
+  end
+
+  test "same_scope/2 should return true for models with the same scope field value" do
+    assert Sample.same_scope(%Sample{scope_field: "same", non_scope_field: "diff0"}, %Sample{scope_field: "same", non_scope_field: "diff1"})
+  end
+
+  test "same_scope/2 should return false for models with different scope field value" do
+    assert !Sample.same_scope(%Sample{scope_field: "diff0"}, %Sample{scope_field: "diff1"})
+  end
+end
