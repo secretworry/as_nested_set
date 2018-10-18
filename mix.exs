7077e64c97dbe147781b88dac28d705637927172
Rainer Du <dusiyh@gmail.com>
2018-10-18 12:25:22 +0800

    Added coveralls.travis to test env

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule AsNestedSet.Mixfile do
      start_permanent: Mix.env == :prod,
      deps: deps(),
      test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+     preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test, "coveralls.travis": :test]
      ]
   end
 
